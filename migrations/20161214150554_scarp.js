12b9ed5795aa316b8fb74743dce54e70e2e742d4
Spencer Scarpelli <spencerscarpelli@gmail.com>
2016-12-14 15:26:17 -0800

    add db migrations

--- migrations/20161214150554_scarp.js
+++ migrations/20161214150554_scarp.js
@@ -9,10 +9,19 @@ exports.up = function(knex, Promise) {
       table.string('github_url');
       table.string('deployment_url');
     }),
-    knex.schema.createTable('posts', function)
-  ])
-};
+    knex.schema.createTable('posts', function(table) {
+      table.increments().primary();
+      table.timestamps(date);
+      table.string('category');
+      table.string('title');
+      table.string('content');
+    })
+  ]);
+}
 
 exports.down = function(knex, Promise) {
-
-};
+  return Promise.all([
+    knex.schema.dropTable('projects'),
+    knex.schema.dropTable('posts')
+  ]);
+}
